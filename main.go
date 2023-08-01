15d12432e859feba73f17cec6c46d7e5575df402
undeath-cyber <gabrielxjonathan.12@gmail.com>
2023-08-01 13:57:14 +0700

    test(api): test connection db

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
+	dsn := "root:lAIBd3XARkGmNncTc1dT@tcp(containers-us-west-194.railway.app:6005)/railway?charset=utf8mb4&parseTime=True&loc=Local"
+	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	fmt.Println("success connect to db")
+}
